fee310ffcdc2ce95cdadcd56a39eb651ad54bc01
Амгалан Борголов <amborgolov007@gmail.com>
2025-05-15 00:41:34 +0300

    tried fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'workouts/:id',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'trainees/:traineeId',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
